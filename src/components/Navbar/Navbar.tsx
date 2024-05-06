19a81a1c88042b462e06eb12cd430eb90b560a3f
mcagdas <mcagdasbayirli@gmail.com>
2024-05-06 10:08:48 +0300

    mobile menu added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -96,6 +96,25 @@ const Navbar: FC<NavbarProps> = ({ whiteLogo }) => {
         className={styles["mobile-menu-container"]}
         style={{ display: isMobileMenuOpen ? "block" : "none" }}
       >
+        <button
+          className={styles["mobile-menu-button"]}
+          onClick={() => setIsMobileMenuOpen(false)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
         <div className={styles["mobile-navbar"]}>
           {NAVBAR_LINKS.map((link) => {
             return (
